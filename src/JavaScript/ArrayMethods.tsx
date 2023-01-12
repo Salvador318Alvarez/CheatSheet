3379ddc0a3a21ff52c499777406f5bcd1c2c51dd
Salvador Alvarez <111669165+Salvador318Alvarez@users.noreply.github.com>
2023-01-12 05:45:16 -0600

    added .filter

--- src/JavaScript/ArrayMethods.tsx
+++ src/JavaScript/ArrayMethods.tsx
@@ -30,11 +30,38 @@ const ArrayMethods = () => {
             <div className='code'>
                 <code>{'const numbers = [1, 2, 3];'}</code>
                 <code>{'numbers.forEach(number =>'}</code>
-                <code>{'console.log(number))'}    </code>
+                <code>{'console.log(number)'}    </code>
             </div>
             <p>Output:<code>1 2 3</code></p>
            
         </Paper>
+
+        <Paper elevation={7}>
+            <Typography variant="h6" component="h3" gutterBottom>Array.filter()</Typography>
+            <Typography variant='body1' gutterBottom>
+                -calls a provided callbackFn function once for each element in an array, and constructs a new array of all the values for which callbackFn returns a truthy value. 
+            </Typography>
+            <Typography variant='body1' gutterBottom>
+                -Array elements which do not pass the callbackFn test are not included in the new array.
+            </Typography>
+            <Typography variant='body1' gutterBottom>
+                -Parameters: (element, index, array )
+            </Typography>
+            <Typography variant='body1' gutterBottom>
+                -Returns: the new array that meets the condition
+            </Typography>
+            <Typography variant='body1'>
+                Example:
+            </Typography>
+            <div className='code'>
+                <code>{'const words = ["spray", "limit", "elite", "exuberant", "destruction", "present"];'}</code>
+                <code>{'const result = words.filter(word => word.length > 6);'}</code>
+                <code>{'console.log(result)'}</code>
+            </div>
+            <p>Output:<code>Array ["exuberant", "destruction", "present"]</code></p>
+           
+        </Paper>
+
      </Box>
         </>
     );
