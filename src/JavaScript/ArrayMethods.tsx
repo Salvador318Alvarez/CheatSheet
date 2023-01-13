b8ed5eae98e40199606d79922468fb3e067196ab
Salvador Alvarez <111669165+Salvador318Alvarez@users.noreply.github.com>
2023-01-13 05:47:49 -0600

    added .map()

--- src/JavaScript/ArrayMethods.tsx
+++ src/JavaScript/ArrayMethods.tsx
@@ -10,7 +10,7 @@ const ArrayMethods = () => {
     <>
     <Typography variant="h5" component="h2" gutterBottom>Array Methods</Typography> 
      <Box>
-        <Paper elevation={7}>
+        <Paper elevation={7} sx={{paddingLeft:'1vh'}}>
             <Typography variant="h6" component="h3" gutterBottom>Array.forEach()</Typography>
             <Typography variant='body1' gutterBottom>
                 -Executes a function once for each array element
@@ -36,7 +36,7 @@ const ArrayMethods = () => {
            
         </Paper>
 
-        <Paper elevation={7}>
+        <Paper elevation={7} sx={{ paddingLeft:'1vh'}}>
             <Typography variant="h6" component="h3" gutterBottom>Array.filter()</Typography>
             <Typography variant='body1' gutterBottom>
                 -calls a provided callbackFn function once for each element in an array, and constructs a new array of all the values for which callbackFn returns a truthy value. 
@@ -59,9 +59,29 @@ const ArrayMethods = () => {
                 <code>{'console.log(result)'}</code>
             </div>
             <p>Output:<code>Array ["exuberant", "destruction", "present"]</code></p>
-           
         </Paper>
 
+        <Paper elevation={7} sx={{paddingLeft:'1vh'}}>
+            <Typography variant="h6" component="h3" gutterBottom>Array.filter()</Typography>
+            <Typography variant='body1' gutterBottom>
+                -creates a new array populated with the results of calling a provided function on every element in the calling array. 
+            </Typography>
+            <Typography variant='body1' gutterBottom>
+                -Parameters: (element, index, array )
+            </Typography>
+            <Typography variant='body1' gutterBottom>
+                -Returns: a new array with each element being the result of the callback function.
+            </Typography>
+            <Typography variant='body1'>
+                Example:
+            </Typography>
+            <div className='code'>
+                <code>{'const array1 = [1, 4, 9, 16];'}</code>
+                <code>{'const map1 = array1.map(x => x * 2);'}</code>
+                <code>{'console.log(map1);'}</code>
+            </div>
+            <p>Output:<code>// Array [2, 8, 18, 32]</code></p>
+        </Paper>
      </Box>
         </>
     );
