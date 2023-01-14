0332b08a6e3a93bd9c424934690d34e98e581bef
Salvador Alvarez <111669165+Salvador318Alvarez@users.noreply.github.com>
2023-01-14 08:58:36 -0600

    react practice

--- src/JavaScript/JavaScript.tsx
+++ src/JavaScript/JavaScript.tsx
@@ -2,6 +2,12 @@ import { useState } from "react";
 
 const JavaScript = () => {
     const [title, setTitle] = useState('Title');
+    const [events, setEvents] = useState([
+        {title:'Title 1', id:1 },
+        {title:'Title 2', id:2 },
+        {title:'Title 3', id:3 },
+        {title:'Title 4', id:4 },
+    ])
 
     const handleClick = () => {
         if (title == "Title"){
@@ -14,9 +20,14 @@ const JavaScript = () => {
     }
     return ( 
     <>
-    <h1>JavaScript</h1>
-    <button onClick={handleClick}>Change Name</button>
-    <p>{title}</p>
+        <h1>JavaScript</h1>
+        <p>{title}</p>
+        <button onClick={handleClick}>Change Name</button>
+        {events.map((event) => (
+            <section key={event.id}>
+                <h2>{event.title}</h2>
+            </section>
+        ))}
     </> );
 }
  
