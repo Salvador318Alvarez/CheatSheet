5d5e9a757584c15fd02b6ef7319662f1c64744f1
Salvador Alvarez <111669165+Salvador318Alvarez@users.noreply.github.com>
2023-01-13 20:28:10 -0600

    practice with React

--- src/JavaScript/JavaScript.tsx
+++ src/JavaScript/JavaScript.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 const JavaScript = () => {
+    const [title, setTitle] = useState('Title');
+
+    const handleClick = () => {
+        if (title == "Title"){
+             setTitle("cool")
+        }
+       else {
+            setTitle("Title")
+       }
+        
+    }
     return ( 
     <>
     <h1>JavaScript</h1>
+    <button onClick={handleClick}>Change Name</button>
+    <p>{title}</p>
     </> );
 }
  
